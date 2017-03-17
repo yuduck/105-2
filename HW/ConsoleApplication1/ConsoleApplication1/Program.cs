413d5ebdd34157a35881dbbecfa398f95aa99453
user <user@2-4>
2017-03-17 12:19:31 +0800

    HW

---
+++ HW/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var musics = FindMusics();
+
+            for (var j = 0; j < musics.Count; j++) {
+                Console.WriteLine("----------------");
+                Console.WriteLine(musics[j].title);
+                Console.WriteLine(musics[j].name);
+                Console.WriteLine(musics[j].startname);
+                Console.WriteLine(musics[j].endname);
+                Console.WriteLine(musics[j].price);
+                Console.WriteLine(musics[j].create);
+                Console.WriteLine("----------------");
+
+            }
+            Console.ReadLine();
+
+        }
+        public static List<music> FindMusics()
+        {
+            List<music> musics = new List<music>();
+            var xml = XElement.Load(@"D:\0317\HW\music.xml");
+            var musictitle = xml.Descendants("Info").ToList();
+            for (var i = 0; i < musictitle.Count(); i++) {
+                //musictitle[i]
+
+                var musicNode = musictitle[i];
+            }
+            musictitle.Where(x => !x.IsEmpty).ToList().ForEach(musicNode =>
+              {
+                  var title = musicNode.Attribute("title").Value.Trim();
+                  var price= musicNode.Element("showInfo").Element("element").Attribute("price").Value.Trim();
+                  var name = musicNode.Element("showInfo").Element("element").Attribute("locationName").Value.Trim();
+                  var starttime = musicNode.Element("showInfo").Element("element").Attribute("time").Value.Trim();
+                  var endtime = musicNode.Element("showInfo").Element("element").Attribute("endTime").Value.Trim();
+                  music musicData = new music();
+                  musicData.title = title;
+                  musicData.price = price;
+                  musicData.name = name;
+                  musicData.startname = starttime;
+                  musicData.endname = endtime;
+                  musicData.create = DateTime.Now;
+                  musics.Add(musicData);
+              });
+            return musics;
+
+        }
+
+    }
+}
---
+++ HW/ConsoleApplication1/ConsoleApplication1/music.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class music
+    {
+        public string title { get; set; }
+        public string name { get; set; }
+        public string startname { get; set; }
+        public string endname { get; set; }
+        public string price { get; set; }
+        public DateTime create { get; set; }
+    }
+}
