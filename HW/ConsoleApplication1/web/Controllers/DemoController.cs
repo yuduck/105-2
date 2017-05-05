8ed2f7ffba764dbe9fc6f538e1748b963c4a6be5
yuduck <1103105313@gm.kuas.edu.tw>
2017-05-05 12:26:29 +0800

    0505

--- HW/ConsoleApplication1/web/Controllers/DemoController.cs
+++ HW/ConsoleApplication1/web/Controllers/DemoController.cs
@@ -9,7 +9,7 @@ namespace web.Controllers
     public class DemoController : Controller
     {
         // GET: Demo
-        public ActionResult Index()
+        public ActionResult duckIndex()
         {
             var music_data = new Data.Data();
             var music = music_data.ReadMusic();
@@ -21,6 +21,22 @@ namespace web.Controllers
 
             });
             return Content(message);
+
+        }
+        public ActionResult Index()
+        {
+            var music_data = new Data.Data();
+            var music = music_data.ReadMusic();
+            //var message = string.Format("收到{0}筆音樂的資料<br/><br/>", music.Count);
+
+            //music.ForEach(x =>
+            //{
+            //    message += string.Format("標題：{0}<br/>位置:{1}<br/>開始時間：{2}<br/>結束時間:{3}<br/>價錢：{4}<br/>建立時間:{5}<br/><br/>", x.title, x.name, x.starttime, x.endtime, x.price, x.create);
+
+            //});
+            ViewBag.UserName = "Duck";
+
+            return View(music);
 
         }
     }
