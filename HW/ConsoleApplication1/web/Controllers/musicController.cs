07a7c935e519017370622ad1c7ffcc8c19af9eae
yuduck <1103105313@gm.kuas.edu.tw>
2017-06-01 20:35:40 +0800

    0517

---
+++ HW/ConsoleApplication1/web/Controllers/musicController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Data;
+namespace web.Controllers
+{
+    public class musicController : Controller
+    {
+        // GET: music
+        public ActionResult Index(string search = "")
+        {
+            var musicRepository = new Data.Data();
+
+            var musics = musicRepository.ReadMusic();
+            if (!string.IsNullOrEmpty(search))
+                musics = musics
+                    .Where(x =>
+                    x.title.Contains(search) ||
+                    x.name.Contains(search))
+                    .ToList();
+            ViewBag.Search = search;
+            //ViewBag.Stations = stations;
+
+            return View(musics);
+        }
+    }
+}
